d4b75383cd7db29164910feba50af55df92836ee
Tony Aldridge <tony@dabapps.com>
2018-11-04 15:30:22 +0000

    Map generation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 // https://tomassedovic.github.io/roguelike-tutorial/part-2-object-map.html
 extern crate tcod;
+extern crate rand;
 
 use tcod::console::*;
 use tcod::colors;
 use tcod::colors::{Color};
+use rand::Rng;
+use std::cmp;
 
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
@@ -12,8 +15,56 @@ const LIMIT_FPS: i32 = 20;
 const MAP_WIDTH: i32 = 80;
 const MAP_HEIGHT: i32 = 45;
 
+const ROOM_MAX_SIZE: i32 = 10;
+const ROOM_MIN_SIZE: i32 = 6;
+const MAX_ROOMS: i32 = 30;
+
 const COLOR_DARK_WALL: Color = Color {r: 0, g: 0, b: 100};
 const COLOR_DARK_GROUND: Color = Color {r: 50, g: 50, b: 150};
+
+#[derive(Clone, Copy, Debug)]
+struct Rect {
+    x1: i32,
+    y1: i32,
+    x2: i32,
+    y2: i32
+}
+impl Rect {
+    pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
+        Rect { x1: x, y1: y, x2: x + w, y2: y + h }
+    }
+
+    pub fn center(&self) -> (i32, i32) {
+        let center_x = (self.x1 + self.x2) / 2;
+        let center_y = (self.y1 + self.y2) / 2;
+        (center_x, center_y)
+    }
+
+    pub fn intersects_with(&self, other: &Rect) -> bool {
+        (self.x1 <= other.x2) && (self.x2 >= other.x1) &&
+            (self.y1 < other.y2) && (self.y2 >= other.y1)
+    }
+}
+
+fn create_room(room: Rect, map: &mut Map) {
+    for x in (room.x1 + 1)..room.x2 {
+        for y in (room.y1 + 1)..room.y2 {
+            map[x as usize][y as usize] = Tile::empty();
+        }
+    }
+}
+
+fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
+    for x in cmp::min(x1, x2)..(cmp::max(x1, x2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
+    for y in cmp::min(y1, y2)..(cmp::max(y1, y2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
 
 #[derive(Clone, Copy, Debug)]
 struct Tile {
@@ -32,11 +83,38 @@ impl Tile {
 
 type Map = Vec<Vec<Tile>>;
 
-fn make_map() -> Map {
-    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
-    map[30][22] = Tile::wall();
-    map[50][22] = Tile::wall();
-    map
+fn make_map() -> (Map, (i32, i32)) {
+    let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+    let mut rooms = vec![];
+    let mut starting_position = (0, 0);
+
+    for _ in 0..MAX_ROOMS {
+        let w = rand::thread_rng().gen_range(ROOM_MIN_SIZE, ROOM_MAX_SIZE + 1);
+        let h = rand::thread_rng().gen_range(ROOM_MIN_SIZE, ROOM_MAX_SIZE + 1);
+        let x = rand::thread_rng().gen_range(0, MAP_WIDTH - w);
+        let y = rand::thread_rng().gen_range(0, MAP_HEIGHT - h);
+
+        let new_room = Rect::new(x, y, w, h);
+        let failed = rooms.iter().any(|other_room| new_room.intersects_with(other_room));
+        if !failed {
+            create_room(new_room, &mut map);
+            if rooms.is_empty() {
+                starting_position = new_room.center();
+            } else {
+                let (prev_x, prev_y) = rooms[rooms.len() - 1].center();
+                let (new_x, new_y) = new_room.center();
+                if rand::random() {
+                    create_h_tunnel(prev_x, new_x, prev_y, &mut map);
+                    create_v_tunnel(prev_y, new_y, new_x, &mut map);
+                } else {
+                    create_v_tunnel(prev_y, new_y, prev_x, &mut map);
+                    create_h_tunnel(prev_x, new_x, new_y, &mut map);
+                }
+            }
+            rooms.push(new_room);
+        }
+    }
+    (map, starting_position)
 }
 
 struct Object {
@@ -83,8 +161,8 @@ fn main() {
     let mut con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
 
     tcod::system::set_fps(LIMIT_FPS);
-    let map = make_map();
-    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', colors::WHITE);
+    let (map, (player_x, player_y)) = make_map();
+    let player = Object::new(player_x, player_y, '@', colors::WHITE);
     let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', colors::YELLOW);
     let mut objects = [player, npc];
 
